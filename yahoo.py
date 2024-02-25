354ccabb3af8d612964056fc2ef382bf2c46fc08
Tom Jammy <threcius@yahoo.com>
2024-02-25 18:28:44 +0800

    Demo on how to render text to widget

--- yahoo.py
+++ yahoo.py
@@ -197,6 +197,7 @@ class YahooButton(Button):
 class MyApp(App):
     def build(self):
         parent = MyWidget()
+        parent.size = Window.size
         #btn = YahooButton(text="MAKE THE ICON", pos=(200, 200), size=(140, 22))
         btn = YahooButton(text="MAKE THE ICON", pos=(200, 200))
         btn2 = YahooButton(text="Comfirm", pos=(200, 175))
@@ -204,6 +205,12 @@ class MyApp(App):
         parent.add_widget(btn)
         parent.add_widget(btn2)
         parent.add_widget(btn3)
+
+        label = Label(text="Hello, World!", pos=(0,0), font_size="12sp", 
+                  color=(1, 0, 0, 1))
+        label.texture_update()
+        label.size = label.texture_size
+        parent.add_widget(label)
         return parent
 
 
